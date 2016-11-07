71a3c1c9c1c0a9cf714e3054b2d939a2dba64a90
Fire_Yw <fire15@126.com>
2016-11-07 10:30:39 +0800

    Create _83. Remove Duplicates from Sorted List.py

---
+++ LeetCode/_83. Remove Duplicates from Sorted List.py
@@ -0,0 +1,26 @@
+# 对链表操作还是不熟悉啊，不知道怎么遍历。一行行分析代码，终于勉强理解了...
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre = head  
+        cur = head.next  
+        while cur != None:                         #当cur节点非空
+            if cur.val != pre.val:               #如果cur的值不等于pre的值，pre和cur指针后移一位
+                pre = cur  
+                cur = cur.next  
+                continue                    #这里可以结束本次循环，那么后面就是cur和pre值相等的情况了           
+            while cur.next != None and cur.next.val == pre.val:          #节约时间，一次性把所有和pre值相同的去掉
+                cur = cur.next  
+            pre.next = cur.next               #这里就消去了cur节点，关键！
+            cur = pre.next                  #继续后面的循环，所以重新生成cur
+        return head 
