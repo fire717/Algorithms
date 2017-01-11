ff4877a6d09c8b4467bf576f4cd5a259952ce64e
Fire_Yw <fire15@126.com>
2017-01-11 12:46:46 +0800

    Create _406. Queue Reconstruction by Height.py

---
+++ LeetCode/_406. Queue Reconstruction by Height.py
@@ -0,0 +1,30 @@
+'''
+Pick out tallest group of people and sort them in a subarray (S).
+Since there's no other groups of people taller than them, therefore each guy's index will be just as same as his k value.
+For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
+'''
+class Solution(object):
+    def reconstructQueue(self, people):
+        if not people: return []
+
+        # obtain everyone's info
+        # key=height, value=k-value, index in original array
+        peopledct, height, res = {}, [], []
+        
+        for i in xrange(len(people)):
+            p = people[i]
+            if p[0] in peopledct:
+                peopledct[p[0]] += (p[1], i),
+            else:
+                peopledct[p[0]] = [(p[1], i)]
+                height += p[0],
+
+        height.sort()      # here are different heights we have
+
+        # sort from the tallest group
+        for h in height[::-1]:
+            peopledct[h].sort()
+            for p in peopledct[h]:
+                res.insert(p[0], people[p[1]])
+
+        return res
