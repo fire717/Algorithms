5414490fcb92c236926eb2cfaf8db2183362a052
Fire_Yw <fire15@126.com>
2017-03-12 11:01:52 +0800

    Update 392. Is Subsequence.py

--- LeetCode/python/392. Is Subsequence.py
+++ LeetCode/python/392. Is Subsequence.py
@@ -12,3 +12,18 @@ class Solution(object):
             else:
                 return False
         return True
+
+#想着改进下，把if判断换成t.find(x)>=0，结果时间变成92ms了，看来if x in str比str.find()效率要高一点
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        for x in s:
+            if t.find(x)>=0:
+                t=t[t.find(x)+1:]
+            else:
+                return False
+        return True
