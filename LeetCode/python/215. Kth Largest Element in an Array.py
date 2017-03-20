f7e90be16e2c0be34398c3d9a614934d9840ff4e
Fire_Yw <fire15@126.com>
2017-03-20 16:53:44 +0800

    Create 215. Kth Largest Element in an Array.py

---
+++ LeetCode/python/215. Kth Largest Element in an Array.py
@@ -0,0 +1,10 @@
+#这不是一道送分题吗...题目中也没说不让用sort呀.....
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        return nums[-k]
