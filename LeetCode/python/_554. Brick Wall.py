89972c5883799fc6a31ebd0f862b414e93b0ab2c
Fire_Yw <fire15@126.com>
2017-05-01 09:35:10 +0800

    Create _554. Brick Wall.py

---
+++ LeetCode/python/_554. Brick Wall.py
@@ -0,0 +1,19 @@
+#cross the least bricks.
+class Solution(object):
+    def leastBricks(self, wall):
+        """
+        :type wall: List[List[int]]
+        :rtype: int
+        """
+        d = collections.defaultdict(int)#dict subclass that calls a factory function to supply missing values。
+        for line in wall:
+            i = 0
+            for brick in line[:-1]:
+                i += brick
+                d[i] += 1
+        # print len(wall), d
+        return len(wall)-max(d.values()+[0])
+        
+'''
+这里的defaultdict(function_factory)构建的是一个类似dictionary的对象，其中keys的值，自行确定赋值，但是values的类型，是function_factory的类实例，而且具有默认值。比如default(int)则创建一个类似dictionary对象，里面任何的values都是int的实例，而且就算是一个不存在的key, d[key] 也有一个默认值，这个默认值是int()的默认值0.
+'''
