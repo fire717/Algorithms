b5e3aaa88977d29aecc815df3233c7eed757d0cd
Fire_Yw <fire15@126.com>
2018-06-19 22:24:05 +0800

    Create 771.JewelsandStones.py

---
+++ LeetCode/python/771.JewelsandStones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        for s in S:
+            if s in J:
+                count+=1
+        return count
