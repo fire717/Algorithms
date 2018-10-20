16ca0ba47b615890451a71a2ffa47ff7426c6411
Fire_Yw <fire15@126.com>
2018-10-20 10:20:29 +0800

    Update 066.PlusOne.py

--- LeetCode/python/066.PlusOne.py
+++ LeetCode/python/066.PlusOne.py
@@ -22,3 +22,48 @@ class Solution(object):
                 else:
                     digits[-i] = 0
         return ans
+
+    
+ # 2018.10.20  云天励飞远程面试
+# 忘了上面n久前的解法，第一思路是转str再转数字再转回来 ，但觉得面试这么写多半不行，于是写了下面：
+# 24 ms - 99.81%
+class Solution:
+    def plusOne(self, nums):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        l = len(nums)
+        foreward = 1
+        for i in range(1,l+1):
+            if nums[-i]+foreward==10:
+                nums[-i] = 0
+                foreward = 1
+            else:
+                nums[-i] += 1
+                foreward = 0
+                break
+        if foreward == 1:
+            nums = [1] + nums
+        return nums
+
+#然后事后查了下网上的解法，就是转字符串的，28ms-88.6%  .
+#明显我的解法更优，但是面试官没任何评价直接就结束了，让我很慌...
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        intNum = 0
+        for i in range(len(digits)):
+            intNum = intNum * 10 + digits[i]
+        intNum += 1
+        # 数字转换成字符
+        strNum = str(intNum)
+        # 字符转换成数组
+        res = []
+        for i in range(len(strNum)):
+            res.append(int(strNum[i]))
+        return res
+
